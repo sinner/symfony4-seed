5fe65d42b073e1f9dc491b30dd1b63c83e735440
Danny Torres <danny.torresxd@gmail.com>
2018-05-11 16:06:36 -0500

    fix issue with the abstract controller using a deleted trait

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Helper\Traits\ControllerEntityServiceTrait;
 use FOS\RestBundle\Controller\FOSRestController;
-
 /**
  * Class AbstractController
  * @package App\Controller
  */
 abstract class AbstractController extends FOSRestController
 {
-	use ControllerEntityServiceTrait;
+    //
 }
