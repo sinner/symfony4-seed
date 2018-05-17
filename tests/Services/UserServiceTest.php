7741aa10a1e309b429551049cfb36e11468f05a6
Danny Torres <danny.torresxd@gmail.com>
2018-05-17 17:19:58 -0500

    first test cases

--- tests/Services/UserServiceTest.php
+++ tests/Services/UserServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Services;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\DataFixtures\AppUsersFixtures;
 use App\Tests\SetupTrait;
@@ -32,16 +33,64 @@ class UserServiceTest extends WebTestCase
     }
 
     /**
+     * Find all the users registered at the database
      * @covers \App\Services\Entity\UserService::findAll
-     *
+     * @group user management
      */
     public function testFindAllUser()
     {
         $users = $this->userService->findAll();
         $this->assertNotEmpty($users);
-
     }
 
+    /**
+     * Search for an user that has a specific username
+     * @covers \App\Services\Entity\UserService::findBy
+     * @group user management
+     */
+    public function testFindAnUserByExistingUsername()
+    {
+        $users = $this->userService->findBy(['username' => 'cory']);
+        $this->assertNotEmpty($users);
+        $this->assertCount(1, $users);
+        $this->assertTrue(is_array($users));
+        $this->assertInstanceOf(User::class, $users[0]);
+    }
+
+    /**
+     * Search for an user that his username does not exists
+     * @covers \App\Services\Entity\UserService::findBy
+     * @group user management
+     */
+    public function testSearchAnUserWithWrongUsername()
+    {
+        $users = $this->userService->findBy(['username' => 'fake-user-name']);
+        $this->assertTrue(is_array($users));
+        $this->assertEmpty($users);
+    }
+
+    /**
+     * Search for an user that his username does not exists and return a null value
+     * @covers \App\Services\Entity\UserService::findOneBy
+     * @group user management
+     */
+    public function testSearchAnUserWithWrongUsernameRetrieveNull()
+    {
+        $user = $this->userService->findOneBy(['username' => 'fake-user-name']);
+        $this->assertNull($user);
+    }
+
+    /**
+     * Search for an user that his username exists and retrieve a user entity
+     * @covers \App\Services\Entity\UserService::findOneBy
+     * @group user management
+     */
+    public function testSearchAnUserWithExistingUsernameRetrieveEntity()
+    {
+        $user = $this->userService->findOneBy(['username' => 'cory']);
+        $this->assertNotNull($user);
+        $this->assertInstanceOf(User::class, $user);
+    }
 
     public static function tearDownAfterClass()
     {
